27d82bcb5c54a9132fa660acb06985ad641d002f
immortalmind2016 <immortal.mind2016@gmail.com>
2020-03-20 00:51:44 +0200

    du test was added

--- model/Applicant_profile.js
+++ model/Applicant_profile.js
@@ -51,121 +51,146 @@ const Applicant_profile=new Schema({
     questions:{
         type:Object,
         default:{
-            en: [
-              { title: "Can you tell me a little about yourself?", link: "" },
-              {
-                title:
-                  "Tell me about a challenge or conflict you've faced at work or at life, and how you dealt with it.",
-                link: ""
-              },
-              { title: "How would your boss and co-workers describe you?", link: "" },
-              { title: "What is your greatest accomplishment?", link: "" },
-              { title: "What are your goals for the future?", link: "" }
-            ],
-            sp: [
-              { title: "¿Podría hablarme sobre usted?", link: "" },
-              { title: "¿Cuáles son sus mayores virtudes o fortalezas?", link: "" },
-              {
-                title: "¿Dónde se ve en un futuro? ¿Cuáles son sus objetivos laborales?",
-                link: ""
-              },
-              {
-                title:
-                  "Hábleme de algún problema al que se haya enfrentado y cómo lo resolvió",
-                link: ""
-              },
-              { title: "¿Cuál ha sido su mayor logro profesional?", link: "" }
-            ],
-            ge: [
-              { title: "Erzählen Sie mir etwas über sich.", link: "" },
-              { title: "Was sind Ihre Stärken? Was sind Ihre Schwächen?", link: "" },
-              {
-                title:
-                  "Was war für Sie bisher die größte Herausforderung in Ihrem Berufsleben?",
-                link: ""
-              },
-              {
-                title:
-                  "Wenn Sie wählen könnten, an welchem Ort der Erde würden Sie jetzt am liebsten sein?",
-                link: ""
-              },
-              { title: "Wo sehen Sie sich in fünf Jahren?", link: "" }
-            ],
-            fr: [
-              { title: "Parlez-moi de vous.", link: "" },
-              { title: "Où vous voyez-vous dans cinq ans ?", link: "" },
-              { title: "Quels sont vos centres d’intérêt ?", link: "" },
-              { title: "Quels sont vos points forts ?", link: "" },
-              { title: "Quels sont vos points forts ?", link: "" }
-            ],
-            it: [
-              { title: `Puoi dirmi qualcosa di te?`, link: `` },
-              {
-                title: `parlami di quella volta in cui hai dovuto affrontare dei conflitti al lavoro o nella vita?`,
-                link: ``
-              },
-              { title: `dove ti vedi tra 5 anni?`, link: `` },
-              {
-                title: `Qual è il tuo più grande successo nella vita o lavoro?`,
-                link: ``
-              },
-              { title: `Come descrivere se stessi?`, link: `` }
-            ],
-            ch: [
-              {
-                title: `你能告诉我一些关于你自己的事吗？`,
-                link: ""
-              },
-              { title: `你认为什么是你的优点和缺点？`, link: "" },
-              { title: `告诉我一个你在工作上克服挑战的经验。- 还是生活`, link: "" },
-              { title: `你对你未来五年怎么打算呢?`, link: "" },
-              { title: `你的事业目标是什么？`, link: "" }
-            ],
-            Ja: [
-              { title: `をおいいたします。`, link: "" },
-              { title: `→ あなたの/をえてください。`, link: "" },
-              { title: `→ のはありますか。`, link: "" },
-              { title: `→ あなたのについてえてください。`, link: "" },
-              { title: `→ でたことはですか。`, link: "" }
-            ],
-            ru: [
-              { title: `пожалуйста, представьтесь?`, link: "" },
-              {
-                title: `Расскажите мне о вызове или конфликте, с которым вы столкнулись на работе или в жизни, и о том, как вы с ним справились.`,
-                link: ""
-              },
-              { title: `Как бы вы описали себя?`, link: "" },
-              { title: `какой у тебя план на будущее?`, link: "" },
-              { title: `Какое ваше самое большое достижение?`, link: "" }
-            ],
-            po: [
-              { title: `Powiedz mi coś o sobie`, link: "" },
-              {
-                title: `Jakie są Twoje mocne strony? I Jakie są Twoje słabe strony?`,
-                link: ""
-              },
-              { title: `Co chciałbyś robić za 5 lat?`, link: "" },
-              {
-                title: `Jaki etap w Twoim życiu był dla Ciebie najtrudniejszy? Jak sobie poradziłeś?`,
-                link: ""
-              },
-              { title: `Co byś zrobił, gdybyś wygrał na loterii?`, link: "" }
-            ],
-            br: [],
-            du: [
-              { title: "Vertel eens iets over uzelf.", link: "" },
-              {
-                title: "Wat zijn uw sterke kanten? Wat zijn uw zwakke kanten?",
-                link: ""
-              },
-              { title: "Wat hebt u tot nu toe bereikt?", link: "" },
-              {
-                title:
-                  "Wat is de moeilijkste situatie die u op uw werk heeft meegemaakt ? of leven?",
-                link: ""
-              },
-              { title: "Wat zijn uw toekomstdoelen?", link: "" }
-            ],
+          en: [
+            { title: "Can you tell me a little about yourself?", link: "" },
+            {
+              title:
+                "Tell me about a challenge or conflict you've faced at work or at life, and how you dealt with it.",
+              link: ""
+            },
+            { title: "How would your boss and co-workers describe you?", link: "" },
+            { title: "What is your greatest accomplishment?", link: "" },
+            { title: "What are your goals for the future?", link: "" }
+          ],
+          sp: [
+            { title: "¿Podría hablarme sobre usted?", link: "" },
+            { title: "¿Cuáles son sus mayores virtudes o fortalezas?", link: "" },
+            {
+              title: "¿Dónde se ve en un futuro? ¿Cuáles son sus objetivos laborales?",
+              link: ""
+            },
+            {
+              title:
+                "Hábleme de algún problema al que se haya enfrentado y cómo lo resolvió",
+              link: ""
+            },
+            { title: "¿Cuál ha sido su mayor logro profesional?", link: "" }
+          ],
+          fr: [
+            { title: "Parlez-moi de vous.", link: "" },
+            { title: "Où vous voyez-vous dans cinq ans ?", link: "" },
+            { title: "Quels sont vos centres d’intérêt ?", link: "" },
+            { title: "Quels sont vos points forts ?", link: "" },
+            { title: "Quels sont vos points forts ?", link: "" }
+          ],
+          ge: [
+            { title: "Erzählen Sie mir etwas über sich.", link: "" },
+            { title: "Was sind Ihre Stärken? Was sind Ihre Schwächen?", link: "" },
+            {
+              title:
+                "Was war für Sie bisher die größte Herausforderung in Ihrem Berufsleben?",
+              link: ""
+            },
+            {
+              title:
+                "Wenn Sie wählen könnten, an welchem Ort der Erde würden Sie jetzt am liebsten sein?",
+              link: ""
+            },
+            { title: "Wo sehen Sie sich in fünf Jahren?", link: "" }
+          ],
+        
+          it: [
+            { title: `Puoi dirmi qualcosa di te?`, link: `` },
+            {
+              title: `parlami di quella volta in cui hai dovuto affrontare dei conflitti al lavoro o nella vita?`,
+              link: ``
+            },
+            { title: `dove ti vedi tra 5 anni?`, link: `` },
+            {
+              title: `Qual è il tuo più grande successo nella vita o lavoro?`,
+              link: ``
+            },
+            { title: `Come descrivere se stessi?`, link: `` }
+          ],
+          br: [], //---//
+        
+          ch: [
+            {
+              title: `你能告诉我一些关于你自己的事吗？`,
+              link: ""
+            },
+            { title: `你认为什么是你的优点和缺点？`, link: "" },
+            { title: `告诉我一个你在工作上克服挑战的经验。- 还是生活`, link: "" },
+            { title: `你对你未来五年怎么打算呢?`, link: "" },
+            { title: `你的事业目标是什么？`, link: "" }
+          ],
+          du: [
+            { title: "Vertel eens iets over uzelf.", link: "" },
+            {
+              title: "Wat zijn uw sterke kanten? Wat zijn uw zwakke kanten?",
+              link: ""
+            },
+            { title: "Wat hebt u tot nu toe bereikt?", link: "" },
+            {
+              title:
+                "Wat is de moeilijkste situatie die u op uw werk heeft meegemaakt ? of leven?",
+              link: ""
+            },
+            { title: "Wat zijn uw toekomstdoelen?", link: "" }
+          ],
+          ja: [
+            { title: `をおいいたします。`, link: "" },
+            { title: `→ あなたの/をえてください。`, link: "" },
+            { title: `→ のはありますか。`, link: "" },
+            { title: `→ あなたのについてえてください。`, link: "" },
+            { title: `→ でたことはですか。`, link: "" }
+          ],
+        
+          la: [
+            {
+              title: "indica mihi paululum de te",
+              link: ""
+            },
+            {
+              title: "quomodo tu te ipsum describere",
+              link: ""
+            },
+            {
+              title: "Quid enim tuum est consilium futuri",
+              link: ""
+            },
+            {
+              title: "Quod est maxima expletionem",
+              link: ""
+            },
+            {
+              title: "Quam operor vos amplio vestri se",
+              link: ""
+            }
+          ],
+          po: [
+            { title: `Powiedz mi coś o sobie`, link: "" },
+            {
+              title: `Jakie są Twoje mocne strony? I Jakie są Twoje słabe strony?`,
+              link: ""
+            },
+            { title: `Co chciałbyś robić za 5 lat?`, link: "" },
+            {
+              title: `Jaki etap w Twoim życiu był dla Ciebie najtrudniejszy? Jak sobie poradziłeś?`,
+              link: ""
+            },
+            { title: `Co byś zrobił, gdybyś wygrał na loterii?`, link: "" }
+          ],
+          ru: [
+            { title: `пожалуйста, представьтесь?`, link: "" },
+            {
+              title: `Расскажите мне о вызове или конфликте, с которым вы столкнулись на работе или в жизни, и о том, как вы с ним справились.`,
+              link: ""
+            },
+            { title: `Как бы вы описали себя?`, link: "" },
+            { title: `какой у тебя план на будущее?`, link: "" },
+            { title: `Какое ваше самое большое достижение?`, link: "" }
+          ],
           }
           
     },
