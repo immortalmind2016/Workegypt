4aa0ba225b3a24a214ad3bc3eee8058d1257f30e
immortalmind2016 <immortal.mind2016@gmail.com>
2020-03-28 18:54:04 +0200

    uploads was added

---
+++ public/precache-manifest.c45e31905e6b7631c32e2c78f1e9d008.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4bd7ffb4797b4a89ab3490cbf6090cc2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d4434506dfb805a02349",
+    "url": "/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "603ecfaa0a3c4e391734",
+    "url": "/static/css/main.1f282ada.chunk.css"
+  },
+  {
+    "revision": "d4434506dfb805a02349",
+    "url": "/static/js/2.6235402f.chunk.js"
+  },
+  {
+    "revision": "90c76a64dd2e9dbcb939561e67c52fd2",
+    "url": "/static/js/2.6235402f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "603ecfaa0a3c4e391734",
+    "url": "/static/js/main.e369e31c.chunk.js"
+  },
+  {
+    "revision": "c98a26f130cbf1d2a097",
+    "url": "/static/js/runtime-main.1afaba67.js"
+  }
+]);
