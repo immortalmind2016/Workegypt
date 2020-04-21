8cfebe27ed0dddf90e1f62e5c7db83329f1b74d4
immortalmind <immortal.mind2016@gmail.com>
2020-04-21 04:25:40 +0200

    fix chat issue

--- controller/chat/chat.js
+++ controller/chat/chat.js
@@ -5,7 +5,7 @@ const Message=require("../../model/Message")
 const Conversation=require("../../model/Conversation")
 
 const getConversation=async(req,res,err)=>{
-  const messages = await Message.find({conversation:req.params.convid})
+  const messages = await Message.find({conversation:req.params.id})
 
 
   res.json({messages})
