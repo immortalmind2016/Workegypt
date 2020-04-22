cf6273a136f34be8e53cb357bcea6be1674a237d
immortalmind <immortal.mind2016@gmail.com>
2020-04-22 12:48:59 +0200

    get conversation

--- controller/chat/chat.js
+++ controller/chat/chat.js
@@ -7,25 +7,29 @@ const Applicant_profile=require("../../model/Applicant_profile")
 const Company_profile=require("../../model/Company_profile")
 
 const getConversation=async(req,res,err)=>{
-  const messages = await Message.find({conversation:req.params.id})
 
-
-  res.json({messages})
+  const messages = await Message.find({$or:[{from:req.params.id,to:req.params.id},{to:req.params.id,from:req.params.id}]})
+  const applicant=await Applicant_profile.findOne({_id:req.params.id}).populate("user")
+  const company=await Company_profile.findOne({_id:req.params.id}).populate("user")
+  res.json({messages,user_info:applicant?applicant:company})
+  
 }
 const getConversations=async(req,res,err)=>{
-  console.log(req.user)
+
+
   const conversations = await Conversation.find({$or:[{applicant:req.user._id},{company:req.user._id}]})
+
   res.json({conversations})
 }
 const createConversation=async(req,res,err)=>{
-    const applicant=await Applicant_profile.findOne({user:req.params.withid})
-    const company=await Company_profile.findOne({user:req.params.withid})
+    const applicant=await Applicant_profile.findOne({_id:req.params.withid})
+    const company=await Company_profile.findOne({_id:req.params.withid})
     if(company){
       new Conversation({
         info: {
 
           applicant: req.user._id,
-          company: company._id
+          company: company.user
 
       }
       
@@ -49,4 +53,5 @@ module.exports={
     getConversation,
     getConversations,
     createConversation
+    
 }
