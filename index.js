9e6e0b9fd2eb26d09ee75b95e6b5fa2afde989fa
immortalmind <immortal.mind2016@gmail.com>
2020-07-29 23:44:01 +0200

    Port config

--- index.js
+++ index.js
@@ -79,7 +79,7 @@ app.use("/*",(req,res,err)=>{
      res.sendFile(__dirname+"/public/index.html")
    })
 console.log("TEST")
-const PORT = process.env.PORT||800
+const PORT = process.env.PORT||80
 http.listen(PORT, () => {
      console.log(`listining on port number ${PORT}`)
 })
