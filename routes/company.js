d074b50ff4ed64d74b30639def901c986bde31d6
mohamed salah <immortal.mind2016@gmail.com>
2020-11-17 21:07:14 +0200

    admin auth

--- routes/company.js
+++ routes/company.js
@@ -1,6 +1,7 @@
 const Router=require("express").Router()
 const {getCompanyJobs,jobApplicants,editApplicantStatus,applyForJob,cancelJob,openContact,opened,subscribe,getProfiles, getCompanies}=require("../controller/company/company")
 const passport=require("../services/jwtPassport")
+const adminPassport=require("../services/adminPassport")
 
 /*
 Mainurl : /api/company
@@ -42,6 +43,6 @@ Router.get("/opened/:id",passport.authenticate('jwt', { session: false }),opened
 
 Router.post("/subscribe/",passport.authenticate('jwt', { session: false }),subscribe)
 Router.get("/profiles/:skip",passport.authenticate('jwt', { session: false }),getProfiles)
-Router.get("/:skip",passport.authenticate('jwt', { session: false }),getCompanies)
+Router.get("/:skip",adminPassport.authenticate('jwt', { session: false }),getCompanies)
 
 module.exports=Router;
