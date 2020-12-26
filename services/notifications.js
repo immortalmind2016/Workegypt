6c4b075ca2cab067919083b0efe4d944aca483cb
mohamed salah <immortal.mind2016@gmail.com>
2020-12-26 20:48:28 +0200

    add notification

--- services/notifications.js
+++ services/notifications.js
@@ -5,6 +5,6 @@ const broadCastNotification = (data) => {
 };
 const sendSocketNotification = (data) => {
     const { io } = require("../index");
-    io.to(data.user).emit(data);
+    io.to(data.user).emit("notification", data);
 };
 module.exports = { broadCastNotification, sendSocketNotification };
