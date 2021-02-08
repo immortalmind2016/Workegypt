53874c0f50cef40eb3ae42f7b4e20ebb37b1fe69
mohamed salah <immortal.mind2016@gmail.com>
2021-02-08 15:58:54 +0200

    fix logout issue

--- controller/user/user.js
+++ controller/user/user.js
@@ -232,7 +232,7 @@ const editUser = async (req, res, err) => {
   console.log("BODY ", req.body.data);
  try{
   let user = await User.findOneAndUpdate({ _id: req.user._id }, { ...req.body.data }, { new: true });
-  if(req.data.password){
+  if(req.body.data.password){
   await user.save();
   }
   return res.json({success:true})
