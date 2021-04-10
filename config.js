afcc2d8c86239e911974023ef28232903ffeaecb
mohamed salah <immortal.mind2016@gmail.com>
2021-04-10 15:13:08 +0200

    url https

--- config.js
+++ config.js
@@ -1,25 +1,25 @@
 module.exports = {
-    url:"http://www.workegypt.net/",
-    //url: "https://workegyptv2.herokuapp.com/",
-    username: "admin",
-    notifications: {
-        checkNewMessage:{
-            title: "Conversations",
-            body: `Check your new messages`
-        },
-        applyForJob: {
-            title: "Applicant applied",
-            body: (title, name) =>
-                `New Application on ${title} job from user ${name}`,
-        },
-        editApplicantStatus: {
-            title: "Job title ",
-            body: "job status ",
-        },
-        cancelJob: {
-            title: "Application cancelled",
-            body: (title, name) =>
-                `Job Application ${title} was cancelled from user ${name}`,
-        },
+  url: "https://www.workegypt.net/",
+  //url: "https://workegyptv2.herokuapp.com/",
+  username: "admin",
+  notifications: {
+    checkNewMessage: {
+      title: "Conversations",
+      body: `Check your new messages`,
     },
+    applyForJob: {
+      title: "Applicant applied",
+      body: (title, name) =>
+        `New Application on ${title} job from user ${name}`,
+    },
+    editApplicantStatus: {
+      title: "Job title ",
+      body: "job status ",
+    },
+    cancelJob: {
+      title: "Application cancelled",
+      body: (title, name) =>
+        `Job Application ${title} was cancelled from user ${name}`,
+    },
+  },
 };
